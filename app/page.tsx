589667e390ed2761be8cd8e1bfaf7b42714a923a
bert093 <sas992203@gmail.com>
2026-04-22 16:37:17 +0800

    feat: update homepage layout and content
    Add Geist font variants, introduce a sticky header, improve the hero
    section styling, and replace the placeholder text with Capstone Project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,43 @@
+import { Geist } from 'next/font/google' 
+
+const geistNormal = Geist({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+const geistMedium = Geist({
+  subsets: ['latin'],
+  weight: '500'
+})
+
+const geistSemibold = Geist({
+  subsets: ['latin'],
+  weight: '600'
+})
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div className={geistNormal.className}>
+        <div className='m-2 sticky top-0 flex justify-between'>
+          <p>Capstone Project</p>
+          <p>Index</p>
+          <p>©Bumigora University</p>
+        </div>
+      </div>
+      <hr className='border-black/30 border'/>
+      <div className={geistSemibold.className}>
+        <div className='flex justify-center items-center text-center min-h-screen text-8xl'>
+          <p>Capstone Project</p>
+        </div>
+      </div>
+
+      <div className={geistMedium.className}>
+        <div className='text-7xl'>
+          <p>We are Computer Science students from Bumigora University, building our Capstone Project with a vision to create something impactful. I lead the project, combining Full-Stack Development and DevOps expertise to bring the idea to life.</p>
+          <p>--- ..- .-. ....... .--. .-. --- ..-. . ... ... --- .-. ... ....... -. . ...- . .-. ....... - .- ..- --. .... - ....... ..- ... ....... .- -... --- ..- - ....... ..-. ..- .-.. .-.. -....- ... - .- -.-. -.- ....... .- -. -.. ....... -.. . ...- --- .--. ... --..-- ....... ... --- ....... .-- . ....... .... .- -.. ....... - --- ....... - . .- -.-. .... ....... --- ..- .-. ... . .-.. ...- . ... .-.-.- ....... -... ..- - ....... -.-- . .- .... .-.-.- .-.-.- .-.-.- ....... .-.. . - ... ....... -.- . . .--. ....... - .... .- - ....... .- ....... ... . -.-. .-. . - .-.-.- .-.-.- .-.-.-</p>
+        </div>
+      </div>
     </main>
   );
 }
